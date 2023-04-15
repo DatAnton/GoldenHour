91ef5129c29639508f523914dfbda21adaf744d0
anton_datsenko <anton.datsenko@wapice.com>
2023-04-15 20:49:13 +0300

    added maui project template

---
+++ GoldenHour.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GoldenHour.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
