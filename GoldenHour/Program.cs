45866fe5d905618f3be2c2a2b36dc4ede35ef038
anton_datsenko <anton.datsenko@wapice.com>
2023-04-29 13:54:31 +0300

    added auth functionality to swagger

--- GoldenHour/Program.cs
+++ GoldenHour/Program.cs
@@ -1,4 +1,5 @@
 using GoldenHour.Extensions;
+using Microsoft.OpenApi.Models;
 
 namespace GoldenHour
 {
@@ -10,7 +11,33 @@ namespace GoldenHour
 
             builder.Services.AddControllers();
 
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "GoldenHour API", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
 
             builder.AddDbServices();
             builder.AddAuthServices();
